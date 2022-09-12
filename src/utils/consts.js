4ab2e507fd69106ffcc24a3b5a9a95ef1820c7be
siddhantloya <siddhant@trell.in>
2022-09-12 20:17:31 +0530

    feat: mobile responsiveness

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,11 +1,11 @@
 export const DROPDOWN_OPTIONS = {
   1: { name: 'Notion', logo: './notion-2.svg' },
-  2: { name: 'NopenSpeed', logo: './apple.svg' },
-  3: { name: 'Noimics', logo: './notion-2.svg' },
-  4: { name: 'NoyerBooks', logo: './notion-2.svg' },
-  5: { name: 'Slack', logo: './slack-new-logo.svg' },
-  6: { name: 'Jira', logo: './jira-3.svg' },
-  7: { name: 'Azure', logo: './azure-1.svg' },
+  2: { name: 'Slack', logo: './slack-new-logo.svg' },
+  3: { name: 'Jira', logo: './jira-3.svg' },
+  4: { name: 'Azure', logo: './azure-1.svg' },
+  5: { name: 'NopenSpeed', logo: './apple.svg' },
+  6: { name: 'Noimics', logo: './notion-2.svg' },
+  7: { name: 'NoyerBooks', logo: './notion-2.svg' },
 };
 
 export const MAX_PRODUCTS = 4;
